5934434474218e392ee90d963e02d8ccab492564
Luc Perin Panta <lucapameni@gmail.com>
2021-07-26 22:10:59 +0100

    modiying setting for css to work in production0

--- solarProject/settings.py
+++ solarProject/settings.py
@@ -161,17 +161,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # new7
 SITE_ID = 1 #7
 
 AUTH_USER_MODEL = 'users.User'
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
+
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_ALL_ORIGINS = True
